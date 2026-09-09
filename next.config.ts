a8621f9ad3e9a6ccebe3f51fb1dc59eab1f988e8
brayanCO299 <brayancarrascoolivos@gmail.com>
2026-09-09 15:03:37 -0500

    Restaura código de la banda1

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'qozsnbtnybetqyvvvfzb.supabase.co',
-        port: '',
-        pathname: '/storage/v1/object/public/**',
+        protocol: "https",
+        hostname: "qozsnbtnybetqyvvvfzb.supabase.co",
+        port: "",
+        pathname: "/storage/v1/object/public/**",
       },
     ],
   },
